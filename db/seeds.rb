ae92e83c5e324480e1623fe52009fc683def6090
ethanmasters <emasters@tamu.edu>
2023-09-10 16:45:54 -0500

    Edited seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,30 @@ events = [
     date: Date.parse('2023-09-10'),
     time: Time.parse('2000-01-01 16:11:00 UTC'),
     location: 'Testing Center',
-    show: true
   },
   {
     name: 'Test Event 2',
     date: Date.parse('2023-09-13'),
     time: Time.parse('2000-01-01 20:14:00 UTC'),
     location: 'Location 2',
-    show: true
   },
   {
     name: 'Test Event 3',
     date: Date.parse('2023-11-06'),
     time: Time.parse('2000-01-01 05:17:00 UTC'),
     location: 'Zach',
-    show: true
   },
   {
     name: 'Test Event 4',
     date: Date.parse('2023-12-05'),
     time: Time.parse('2000-01-01 04:14:00 UTC'),
     location: 'The Moon',
-    show: true
   },
   {
     name: 'Test Event 5',
     date: Date.parse('2023-09-10'),
     time: Time.parse('2000-01-01 16:13:00 UTC'),
     location: 'TBD',
-    show: true
   }
 ]
 
