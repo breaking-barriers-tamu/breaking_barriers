8f48f79635826828e19cb5280567a258e7cc6f52
Jacob Rasmussen <jrr2024@tamu.edu>
2023-09-12 08:50:50 -0500

    add validations to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
+<<<<<<< Updated upstream
     validates :name, presence: true
+=======
+    validates :name, :location, :date, presence: true
+>>>>>>> Stashed changes
 end
