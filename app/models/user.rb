6c26a37b949449dc36925148f95eb3b899a17c81
Jason <lejason123@gmail.com>
2023-09-22 23:55:50 -0500

    tamu.edu restriction

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(auth)
+    return nil unless auth.info.email =~ /@tamu.edu\z/
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
@@ -14,3 +15,4 @@ class User < ApplicationRecord
   end
   
 end
+
