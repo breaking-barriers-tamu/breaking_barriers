a1aa7a2962e4ff4827942814649074ce46eb066b
Jason <lejason123@gmail.com>
2023-09-23 01:36:35 -0500

    remove testing

--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -1,25 +1,25 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "Admins", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/admin/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
+# RSpec.describe "Admins", type: :request do
+#   describe "GET /index" do
+#     it "returns http success" do
+#       get "/admin/index"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
 
-  describe "GET /events" do
-    it "returns http success" do
-      get "/admin/events"
-      expect(response).to have_http_status(:success)
-    end
-  end
+#   describe "GET /events" do
+#     it "returns http success" do
+#       get "/admin/events"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
 
-  describe "GET /show_event" do
-    it "returns http success" do
-      get "/admin/show_event"
-      expect(response).to have_http_status(:success)
-    end
-  end
+#   describe "GET /show_event" do
+#     it "returns http success" do
+#       get "/admin/show_event"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
 
-end
+# end
