8d681ac87edbfe54ac4e03a58994de5d2c3003cb
Jason <lejason123@gmail.com>
2023-09-23 01:32:42 -0500

    add admin controller, checks for admin to enter dashboard

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,10 @@
+class AdminController < ApplicationController
+  def index
+  end
+
+  def events
+  end
+
+  def show_event
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do 
+  authenticated :user, ->(user) { user.admin? } do 
+    get 'admin', to: 'admin#index'
+    get 'admin/events'
+    get 'admin/show_event'
+  end
   root 'pages#home'
 
   get 'home', to: 'pages#home'
