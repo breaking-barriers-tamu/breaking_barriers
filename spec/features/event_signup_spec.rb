7d0757bd52177d1303a65b4df7bfdaeb5338c628
Jason <lejason123@gmail.com>
2023-09-25 16:36:39 -0500

    add first,lastname to testcase

--- spec/features/event_signup_spec.rb
+++ spec/features/event_signup_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'Event signup', type: :feature do
   let!(:event) { create(:event, name: 'Existing Event', date: '2023-12-31', time: '15:30:00', location: 'Event Location') }
-  let!(:user) { create(:user, access_level: 2, registration_completed: true) }
+  let!(:user) { create(:user, first_name: 'first_name_1', last_name: 'last_name_1', access_level: 2, registration_completed: true) }
 
   scenario 'Sign up for an event' do
     sign_in(user)
