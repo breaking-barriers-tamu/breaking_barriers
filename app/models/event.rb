c8559565a142ff727631d7261e6cfd85b73e72f3
JuanC <juan.carrascoluis@gmail.com>
2023-09-25 13:07:56 -0500

    destroy event and dependent event_logs

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
     has_many :users, through: :event_logs
+    has_many :event_logs, dependent: :destroy
+    
     validates :name, :date, :time, :location, :duration, presence: true
 end
