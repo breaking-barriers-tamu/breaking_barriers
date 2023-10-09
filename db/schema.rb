814a0327821ea27b3be7a8e8ac5cd560ebee6c4b
JuanC <juan.carrascoluis@gmail.com>
2023-10-09 13:38:57 -0500

    add_column migration to Events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_100035) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_09_182154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_100035) do
     t.datetime "updated_at", null: false
     t.string "description"
     t.decimal "duration"
+    t.boolean "event_enabled"
+    t.integer "officer_in_charge"
   end
 
   create_table "users", force: :cascade do |t|
