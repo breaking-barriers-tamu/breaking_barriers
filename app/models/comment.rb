110b7c261d2a55de9a00c2c3a6951c7301028ccf
Ethan Masters <emasters@tamu.edu>
2023-10-12 10:52:28 -0500

    fixed brakeman error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :announcement
   belongs_to :user
 
-  validates :content, presence: true, format: { with: /\S/ }
+  validates :content, presence: true, format: { with: /\A\S*\z/ }
 end
