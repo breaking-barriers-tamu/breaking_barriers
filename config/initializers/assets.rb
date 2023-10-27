42287a33f8a942986de23c892fe8e9df65fa44e0
Jason <lejason123@gmail.com>
2023-10-27 01:09:27 -0500

    finish styling footer, added images, dynamic footer displayed for specifc screen

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,8 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+
+Rails.application.config.assets.precompile += %w( EnMed.png )
+Rails.application.config.assets.precompile += %w( map-solid.svg )
+Rails.application.config.assets.precompile += %w( phone-solid.svg )
