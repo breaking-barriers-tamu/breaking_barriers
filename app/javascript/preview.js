9ab355538058ff5435faeb5aedd307feec63198c
JuanC <juan.carrascoluis@gmail.com>
2023-10-31 12:40:07 -0500

    If user doesn't upload file, don't preview a file

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,9 +2,12 @@ function readURL(input) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
     reader.onload = function(e) {
-      $(document.getElementById(input.id + "_medium")).attr('src', e.target.result);
+      $(document.getElementById("file_preview")).attr('src', e.target.result);
     }
     reader.readAsDataURL(input.files[0]);
+  }
+  else {
+    $(document.getElementById("file_preview")).attr('src', '');
   }
 }
 
