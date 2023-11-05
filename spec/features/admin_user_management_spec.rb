4f3eec72db4b6a3b282c8b4d50741622c5cf2c80
WilliamTichenor <wctich@gmail.com>
2023-11-04 23:40:28 -0500

    made tests more bearble

--- spec/features/admin_user_management_spec.rb
+++ spec/features/admin_user_management_spec.rb
@@ -3,9 +3,9 @@
 require 'rails_helper'
 
 describe 'User Management', type: :feature do
-  let!(:user) { create(:user, first_name: 'B', last_name: 'Y', access_level: 1, registration_completed: true) }
-  let!(:user2) { create(:user, first_name: 'B', last_name: 'G', access_level: 0, registration_completed: true) }
-  let!(:user3) { create(:user, first_name: 'A', last_name: 'Z', access_level: 0, registration_completed: true) }
+  let!(:user) { create(:user, first_name: 'Baba', last_name: 'You', access_level: 1, registration_completed: true) }
+  let!(:user2) { create(:user, first_name: 'Baba', last_name: 'Green', access_level: 0, registration_completed: true) }
+  let!(:user3) { create(:user, first_name: 'Ari', last_name: 'Zona', access_level: 0, registration_completed: true) }
   let!(:event) { create(:event, name: 'Event 1', datetime: '2001-12-31 15:30:00', location: 'Event Location') }
   let!(:event2) { create(:event, name: 'Event 2', datetime: '2201-12-31 15:30:00', location: 'Event Location') }
   let!(:event_log1) { create(:event_log, event_id: event.id, user_id: user.id, hours: 0.5, participating: true) }
@@ -18,7 +18,7 @@ describe 'User Management', type: :feature do
   it 'sunny day - admin visits page' do
     sign_in(user)
     visit admin_users_path
-    expect(page.text).to match(/A[\S\s]*Z[\S\s]*B[\S\s]*G[\S\s]*B[\S\s]*Y/)
+    expect(page.text).to match(/Ari[\S\s]*Zona[\S\s]*Baba[\S\s]*Green[\S\s]*Baba[\S\s]*You/)
     expect(page).to(have_content(user.first_name))
     expect(page).to(have_content(user2.first_name))
     expect(page).to(have_content(user3.first_name))
