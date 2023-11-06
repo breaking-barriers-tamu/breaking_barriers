8c5c05dcf1f9d6fd93554df126c056d0c9d62d49
JuanC <juan.carrascoluis@gmail.com>
2023-11-06 09:18:07 -0600

    Fix test cases

--- spec/features/event_signup_spec.rb
+++ spec/features/event_signup_spec.rb
@@ -9,7 +9,7 @@ describe 'Event signup', type: :feature do
   it 'Sign up for an event-- Sunny day' do
     sign_in(user)
     visit event_path(event)
-    click_on 'Sign up for this event'
+    click_link 'Sign Up'
     expect(page).to(have_content('You are signed up for this event!'))
     expect(EventLog.where(user: user, event: event)).not_to(be_empty)
   end
@@ -17,7 +17,7 @@ describe 'Event signup', type: :feature do
   it 'Sign up for an event present but already signed up-- Rainy day' do
     sign_in(user)
     visit event_path(event)
-    click_on 'Sign up for this event'
+    click_link 'Sign Up'
     expect(page).not_to(have_content('Sign up for this event'))
     expect(EventLog.where(user: user, event: event)).not_to(be_empty)
   end
@@ -25,9 +25,9 @@ describe 'Event signup', type: :feature do
   it 'Remove myself from an event-- Sunny day' do
     sign_in(user)
     visit event_path(event)
-    click_on 'Sign up for this event'
+    click_link 'Sign Up'
     visit event_path(event)
-    click_on 'Remove me from this event'
+    click_link 'Remove Me'
     expect(page).to(have_content('Successfully removed you from this event.'))
     expect(EventLog.where(user: user, event: event)).to(be_empty)
   end
