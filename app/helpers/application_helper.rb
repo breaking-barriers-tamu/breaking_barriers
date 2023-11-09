4c85c229d50f7e508ef4c5defb9aa8c9927e0074
Jason <lejason123@gmail.com>
2023-11-08 21:31:14 -0600

    show announcement styling enhancements, truncate description in announcment index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+    def truncate_text(text, length = 100)
+        plain_text = text.to_plain_text # this is to get raw text from RichText
+        plain_text.truncate(length, separator: ' ', omission: '...')
+    end
 end
