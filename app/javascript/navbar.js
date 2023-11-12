7d885adc0697abae4611eff8c04055b28da34125
Jason <lejason123@gmail.com>
2023-11-11 23:49:37 -0600

    enhance styling for navbar for home screen

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -3,14 +3,33 @@ function updateNavbarBgColor() {
   const navbar = document.querySelector(".nav-container");
   const navbarShade = document.querySelector(".nav-shade");
   const isHomePage = document.body.getAttribute("data-page") === "home";
+  const isAboutPage = document.body.getAttribute("data-page") === "about";
+
 
   const loginButton = document.querySelector(".login-button");
+  const scrollBounds = 200;
 
-  if (window.scrollY < 100 && isHomePage) {
+  if (isHomePage) {
+    const coverTitle = document.querySelector(".title-container");
+    const navLogo = document.querySelector(".logo");
+    if (window.scrollY < scrollBounds) {
+      coverTitle.style.display = "block";
+      navLogo.style.opacity = "0";
+    } else {
+      coverTitle.style.display = "none";
+      navLogo.style.opacity = "1";
+    }
+  }
+
+  if (
+    (window.scrollY < scrollBounds && isHomePage) ||
+    (window.scrollY < scrollBounds && isAboutPage)
+  ) {
     navbar.style.backgroundColor = "transparent";
-    navbarShade.style.background = "linear-gradient(to bottom, rgba(0,0,0,.6) 0%, rgba(0,0,0,0) 100%)";
-    if (loginButton) loginButton.style.background = "#4872AE"; 
-    if (loginButton) loginButton.style.color = "white"; 
+    navbarShade.style.background =
+      "linear-gradient(to bottom, rgba(0,0,0,.6) 0%, rgba(0,0,0,0) 100%)";
+    if (loginButton) loginButton.style.background = "#4872AE";
+    if (loginButton) loginButton.style.color = "white";
   } else {
     navbar.style.backgroundColor = "#4872AE";
     navbarShade.style.background = "transparent";
@@ -18,7 +37,7 @@ function updateNavbarBgColor() {
     if (loginButton) loginButton.style.color = "black";
   }
   // Adjust padding for home screen
-  document.body.style.paddingTop = isHomePage ? "0" : "70px";
+  document.body.style.paddingTop = isHomePage || isAboutPage ? "0" : "70px";
 }
 
 document.addEventListener("DOMContentLoaded", updateNavbarBgColor);
