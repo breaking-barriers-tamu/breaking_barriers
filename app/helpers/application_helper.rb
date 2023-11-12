b5fe9840fa2404085a1d4c6c940c944a26e8e014
Jason <lejason123@gmail.com>
2023-11-11 23:01:39 -0600

    renmamed truncate_text to summarize text & fix div

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-    def truncate_text(text, length = 100)
+    def summarize_text(text, length = 100)
         plain_text = text.to_plain_text # this is to get raw text from RichText
         plain_text.truncate(length, separator: ' ', omission: '...')
     end
