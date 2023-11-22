b24c24c424b20e478b6d4483da6b09e937cd4adc
JuanC <juan.carrascoluis@gmail.com>
2023-11-22 13:32:42 -0600

    add a possible fix to calendar timezone issue

---
+++ spec/features/event_calendar_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+
+describe 'Event Calendar Feature', type: :feature do
+    let!(:event) {create(:event, name:'Sample Event', description: "Event Description", datetime: '2023-11-22 4:30PM', location: 'Somewhere', duration: 1.0) }
+    let!(:user) { create(:user, phone_number: '(214) 123 - 4567', first_name: 'first', last_name: 'last', access_level: 0, registration_completed: true) }
+    current_month = Date.today
+    
+    it 'Shows an event on the calendar' do
+        sign_in(user)
+        visit events_path
+
+        expect(page).to(have_content('Sample Event'))
+        expect(page).to(have_content('04:30 PM'))
+    end        
+        
+    it 'Highlights the current day' do
+        sign_in(user)
+        visit events_path
+
+        expect(page).to(have_css('.calendar-today'))
+    end
+    
+    it 'Shows the current month' do
+        sign_in(user)
+        visit events_path
+
+        expect(page).to(have_content(current_month.strftime("%B %Y")))
+    end
+
+    it 'Navigates to the previous month' do
+        sign_in(user)
+        visit events_path
+
+        prev_month = current_month.prev_month(1)
+        find('.prev-link').click
+        expect(page).to(have_content(prev_month.strftime("%B %Y")))
+
+    end
+
+    it 'Navigates to the next month' do
+        sign_in(user)
+        visit events_path
+
+        next_month = current_month.next_month(1)
+        find('.next-link').click
+        expect(page).to(have_content(next_month.strftime("%B %Y")))
+    end
+
+end
